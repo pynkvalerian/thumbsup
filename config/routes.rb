d4f458835a8be4d7d01da7eda8546ca849ae3f1e
pynkvalerian <yenping.chew90@gmail.com>
2017-02-14 00:00:23 +0800

    add sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'sign_in', to: 'sessions#new', as: :sign_in
   get '/auth/:provider/callback', to: 'sessions#create'
+  delete 'sign_out', to: 'sessions#destroy', as: :sign_out
 
   resources :thumbs_ups, only: [:new, :create]
 
